3aa64a68a11a1d31959f6987d82d6c81ec427ffa
Sudhir Kumar <37929232+sudhir-codeprism@users.noreply.github.com>
2020-09-21 08:14:29 +0530

    Updates

---
+++ precache-manifest.f91091c8a0ac695471f1557945312b63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aacd070f5cc021028a6dbc54e345279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d45366d25c4bf38b740",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "64d779256b40e8b37fdc",
+    "url": "/static/css/main.63523a45.chunk.css"
+  },
+  {
+    "revision": "8d45366d25c4bf38b740",
+    "url": "/static/js/2.e2ab8517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2ab8517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d779256b40e8b37fdc",
+    "url": "/static/js/main.96cb7f39.chunk.js"
+  },
+  {
+    "revision": "91a46a0099d0057487f0",
+    "url": "/static/js/runtime-main.7870e996.js"
+  }
+]);
--- src/components/Githubcredential.js
+++
@@ -1,2 +0,0 @@
-export const clientId = '9b2e6eb46a8fe42bac72';
-export const clientSecret = 'fc101c6416e3d397a6634213ae3d0358144ed55e'
